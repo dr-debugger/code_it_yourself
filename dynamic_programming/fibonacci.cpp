83b6e895697433f1affd318f83ea7867c3941d4f
arijit <arijitmaiti437@gmail.com>
2023-01-31 12:48:01 +0530

    restructured fibonacci

--- dynamic_programming/fibonacci.cpp
+++ dynamic_programming/fibonacci.cpp
@@ -1,20 +1,38 @@
 #include<iostream>
 #include<string>
+#include<map>
 using namespace std;
 
-class Fib{
-  int *memoized;
 
-  public:
-  Fib(int n){
-    memoized = new int[n];
+class Fib{
+  map<int, double> mapObj;
+
+public:
+  Fib(){
+    mapObj.insert(pair<int, double>(1, 1));
+    mapObj[2] = 1;
   }
-  int fibonacchi(int value){
+  
+  double fibonacchi(int value){
     if(value <= 2){
       return 1;
     }
 
-    return fibonacchi(value - 1) + fibonacchi(value - 2);
+    map<int, double>::iterator itr = mapObj.find(value);
+    if(itr == mapObj.end()){
+      return mapObj[value] = fibonacchi(value - 1) + fibonacchi(value - 2);
+    }
+
+    return itr->second;
+  }
+
+  void displayNmbers(){
+    map<int, double>::iterator itr = mapObj.begin();
+
+    while(itr != mapObj.end()){
+      cout << "key : " << itr->first << " value: " << itr->second << endl;
+      ++itr;
+    }
   }
 
 };
@@ -22,16 +40,31 @@ class Fib{
 
 
 int main(){
-  // int sum = fibonacchi(2);
-  // cout << sum << endl;
 
-  int arr[8];
+  int num;
+  cout << "Enter the number of terms : ";
+  cin >> num;
 
+  Fib f;
+  double sum = f.fibonacchi(num);
+  cout << sum << endl;
+
+  f.displayNmbers();
 
   return 0;
 }
 
 /**
  * sum of n fiboncci numbers
+ *
+ * f(n) = f(n-1) + f(n-2)
+ * we're using map object for memoized the value;
+ * serching for the key in map object in O(log n) complexity;
+ *
+ * for generating the sum of n Numbers the complexity is O(nlogn)
+ * for displaying the n Numbers the complexity is O(n);
  * 
+ * 
+ * **Note** : while generating sum of n Numbers , we are generating fibonacci value of every individual Number.
+ *
  */
