ca407f98156b11e327a788e8f3a2a03e190c626d
arijit <arijitmaiti437@gmail.com>
2023-01-31 11:24:24 +0530

    refactored

--- dynamic_programming/fibonacci.cpp
+++ dynamic_programming/fibonacci.cpp
@@ -2,17 +2,31 @@
 #include<string>
 using namespace std;
 
-int fibonacchi(int value){
-  if(value <= 2){
-    return 1;
+class Fib{
+  int *memoized;
+
+  public:
+  Fib(int n){
+    memoized = new int[n];
+  }
+  int fibonacchi(int value){
+    if(value <= 2){
+      return 1;
+    }
+
+    return fibonacchi(value - 1) + fibonacchi(value - 2);
   }
 
-  return fibonacchi(value - 1) + fibonacchi(value - 2);
-}
+};
+
+
 
 int main(){
-  int sum = fibonacchi(2);
-  cout << sum << endl;
+  // int sum = fibonacchi(2);
+  // cout << sum << endl;
+
+  int arr[8];
+
 
   return 0;
 }
