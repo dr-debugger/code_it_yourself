ae5f4a071b14763ef307a774f6bbaf82bbf02270
arijit <arijitmaiti437@gmail.com>
2023-03-01 23:50:59 +0530

    hackerrank: counting vellys

--- hackerRank/string/counting_valleys.cpp
+++ hackerRank/string/counting_valleys.cpp
@@ -5,25 +5,39 @@ using namespace std;
 int countingValleys(int steps, string path) {
   int strLength = path.length();
   int level = 0; // up from sea level will be 1, down -1, and on sea level it will be 0;
-  bool enteredvalley = false;
+  
   int valleyCount = 0;
 
   for (int i = 0; i < strLength; i++){
-    if(path[i] == 'D')
-      level--;
-    if(path[i] == 'U')
+    if(path[i] == 'U' && (level + 1) == 0 ){
+      valleyCount++;
       level++;
+      continue;
+    }
 
+    if(path[i] == 'U'){
+      level++;
+    }
 
+    if(path[i] == 'D'){
+      level--;
+    }
     
   }
 
-  return 0;
+  return valleyCount;
 }
 
 int main(){
 
-  countingValleys(5, "valley");
+  cout << countingValleys(8, "UDDDUDUU");
 
   return 0;
 }
+
+
+/**
+ * @Ques 
+ * https://www.hackerrank.com/challenges/three-month-preparation-kit-counting-valleys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+ * 
+ */
