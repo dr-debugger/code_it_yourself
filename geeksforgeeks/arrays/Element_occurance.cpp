ae61bc75d143f3d324309d84e11d9bbf2b32f83a
arijit <arijitmaiti437@gmail.com>
2023-04-01 22:16:49 +0530

    staff

---
+++ geeksforgeeks/arrays/Element_occurance.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<map>
+using namespace std;
+
+
+int firstElementKTime(int a[], int n, int k){
+  map<int, int> m;
+
+  cout << m[8] << endl;
+  for (int i = 0; i < n; i++){
+
+  }
+}
+
+int main(){
+  int arr[] = {1, 4, 5};
+  firstElementKTime(arr, 3, 2);
+  return 0;
+}
+
+/**
+ * @brief https://practice.geeksforgeeks.org/problems/first-element-to-occur-k-times5150/1?page=1&curated[]=1&sortBy=submissions
+ * 
+ */
