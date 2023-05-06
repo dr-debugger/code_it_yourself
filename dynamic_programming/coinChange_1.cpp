cc2aa7a6d2241f388680587ccc61b645c227c7a4
arijit <arijitmaiti437@gmail.com>
2023-05-06 19:11:25 +0530

    change in dp

--- dynamic_programming/coinChange_1.cpp
+++ dynamic_programming/coinChange_1.cpp
@@ -1,33 +1,56 @@
-#include<iostream>
-#include<string>
-#include<limits.h>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <map>
+#include <limits.h>
+#include <vector>
 using namespace std;
 
-int countCoin(vector<int> &coins, int target)
+int countCoin(vector<int> &coins, int target, map<int, int> &m)
 {
   if (target == 0)
     return 0;
 
   int ans = INT_MAX;
 
-  for (int i = 0; i < coins.size(); i++)
+  if (m.find(target) != m.end())
   {
-    if (target - coins[i] >= 0)
+    ans = m[target];
+  }
+  else
+  {
+    for (int i = 0; i < coins.size(); i++)
     {
-      int subSum = countCoin(coins, target - coins[i]);
-
-      if (subSum != INT_MAX)
+      if (target - coins[i] >= 0)
       {
-        ans = min(ans, subSum + 1);
+        int subSum = countCoin(coins, target - coins[i], m);
+
+        if (subSum != INT_MAX)
+        {
+          ans = min(ans, subSum + 1);
+        }
       }
     }
   }
 
+  m[target] = ans;
   return ans;
 }
 
-int main(){
-  
+int main()
+{
+
+  map<int, int> m;
+  vector<int> c = {1, 5, 7};
+
+  int a = countCoin(c, 18, m);
+  cout << a;
+
   return 0;
 }
+
+/**
+ * @Ques
+ * Given an integer array of coins[ ] of size N representing different types of currency and an integer sum, The task is to find the number of ways to make sum by using different combinations from coins[]. you can use same coin multiple times;
+ *
+ *
+ */
