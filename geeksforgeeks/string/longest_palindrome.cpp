656eaddd3ebf0689746e86fc57fd3a4fcf94eab1
arijit <arijitmaiti437@gmail.com>
2023-06-15 23:11:24 +0530

    gfg 45

---
+++ geeksforgeeks/string/longest_palindrome.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string longestPalin(string S)
+{
+  int size = S.size(), end = 0, start = 0, length = 0;
+
+  for (int i = 0; i < size; i++)
+  {
+
+    int currLength = 1;
+
+    // check for odd
+    int l = i - 1, r = i + 1;
+    while (l >= 0 && r < size && S[l] == S[r])
+    {
+      currLength += 2;
+      l--;
+      r++;
+    }
+
+    if (currLength > length)
+    {
+      length = currLength;
+      start = l + 1;
+      end = r;
+    }
+
+    // check for even
+    l = i - 1, r = i, currLength = 0;
+    while (l >= 0 && r < size && S[l] == S[r])
+    {
+      currLength += 2;
+      l--;
+      r++;
+    }
+
+    if (currLength > length)
+    {
+      length = currLength;
+      start = l + 1;
+      end = r;
+    }
+  }
+
+  // you got the maximum substring, and also got the start and end index of the substring
+
+  return S.substr(start, length);
+}
+
+int main(){
+  
+  return 0;
+}
