19a125c65bcfb9a35ec1dc2c37beafd37b4e4abc
arijit <arijitmaiti437@gmail.com>
2023-06-25 14:33:34 +0530

    gfg 50

---
+++ geeksforgeeks/arrays/unique_row_in_matrix.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+#define MAX 1000
+
+vector<vector<int>> uniqueRow(int M[MAX][MAX], int row, int col)
+{
+  set<string> unique;
+  vector<vector<int>> res;
+
+  for (int i = 0; i < row; i++)
+  {
+    string temp = "";
+    vector<int> data;
+
+    for (int j = 0; j < col; j++)
+    {
+      data.push_back(M[i][j]);
+      temp += to_string(M[i][j]);
+    }
+
+    if (unique.find(temp) != unique.end())
+    continue;
+
+    unique.insert(temp);
+    res.push_back(data);
+  }
+
+  return res;
+}
+
+int main()
+{
+
+  return 0;
+}
