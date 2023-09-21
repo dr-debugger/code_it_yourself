97a70c0287b3688f3429cf2e85719dc3df4b11a6
arijit <arijitmaiti437@gmail.com>
2023-09-21 12:59:00 +0530

    gfg 80

---
+++ dynamic_programming/stickler_thief.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int FindMaxSum(int arr[], int n)
+{
+  vector<int> dp(n);
+  dp[0] = arr[0];
+  dp[1] = max(arr[0], arr[1]);
+  for (int i = 2; i < n; i++)
+  {
+    dp[i] = max(dp[i - 2] + arr[i], dp[i - 1]);
+  }
+  return dp[n - 1];
+}
+
+int main(){
+  
+  return 0;
+}
+
+/**
+ * @brief
+ * https://practice.geeksforgeeks.org/problems/stickler-theif-1587115621/1
+ *
+ * TC: O(N)
+ * SC: O(1)
+ */
